8f8d4ed8d42db8d8a0ab65f9875ed21a901262df
Meduzz <drgevalia@gmail.com>
2014-07-07 18:08:21 +0200

    Moves the registerService method from ProjectBuilder to ServiceRegistry where it belongs.

--- src/main/scala/se/chimps/bitziness/core/project/ProjectBuilder.scala
+++ src/main/scala/se/chimps/bitziness/core/project/ProjectBuilder.scala
@@ -1,16 +1,12 @@
 package se.chimps.bitziness.core.project
 
-import se.chimps.bitziness.core.service.Service
-
 /**
  * A base DSL for building projects. Individual modules should add their own dsl/builders/settings to this instance.
  */
 object ProjectBuilder {
   def apply(projectRef:Project):ProjectBuilder = new ProjectBuilder {
-    override def addService[T<:Service](service: Class[T]):ProjectBuilder = { projectRef.registerService(service); this }
   }
 }
 
 trait ProjectBuilder {
-  def addService[T<:Service](service:Class[T]):ProjectBuilder
 }
--- src/main/scala/se/chimps/bitziness/core/project/modules/registry/ServiceRegistry.scala
+++ src/main/scala/se/chimps/bitziness/core/project/modules/registry/ServiceRegistry.scala
@@ -1,22 +1,34 @@
 package se.chimps.bitziness.core.project.modules.registry
 
 import se.chimps.bitziness.core.project.modules.Module
-import se.chimps.bitziness.core.project.Project
+import se.chimps.bitziness.core.project.{ProjectBuilder, Project}
 import se.chimps.bitziness.core.service.Service
 import akka.actor.{Props, ActorSystem, ActorRef}
 
 /**
- * Created by meduzz on 05/07/14.
+ * This module helps keeping track of registered services and are automatically extended by the
+ * Project class.
  */
 trait ServiceRegistry extends Module { project:Project =>
   def actorSystem:ActorSystem
 
-  var services:Seq[ActorRef] = Seq()
+  private var services:Seq[ActorRef] = Seq()
 
   def registerService[T<:Service](service:Class[T]) = {
-    services = services ++ Seq(actorSystem.actorOf(Props(service), service.getName))
+    services = services ++ Seq(actorSystem.actorOf(Props(service), s"/project/services/${service.getSimpleName}"))
     // TODO broadcast event of the new service.
   }
 
-  // TODO def broadcast(event:Event) sends event to all services.
+  implicit def registryToProjectHook(builder:ProjectBuilder):Builder = {
+    return new Builder {
+      override def addService[T <: Service](service:Class[T]):ProjectBuilder = {
+        project.registerService(service)
+        builder
+      }
+    }
+  }
+}
+
+trait Builder {
+  def addService[T<:Service](service:Class[T]):ProjectBuilder
 }
