01d168cbc292011ae75c1e71e289cb304fe56faa
Meduzz <drgevalia@gmail.com>
2015-11-21 17:44:58 +0100

    Fixes Riemann, at least some what.

---
+++ src/test/scala/sandboxes/RiemannSandbox.scala
@@ -0,0 +1,30 @@
+package sandboxes
+
+import java.util.concurrent.TimeUnit
+
+import akka.actor.{PoisonPill, Props, ActorSystem}
+import se.chimps.bitziness.core.generic.Init
+import se.chimps.bitziness.core.generic.metrics.{LongMetric, DecimalMetric, BooleanMetric, StringMetric}
+import se.chimps.bitziness.core.services.metrics.adapters.{RiemannSettings, RiemannMetricsService}
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object RiemannSandbox extends App {
+  val system = ActorSystem("RiemannSandbox")
+  val settings = RiemannSettings("192.168.235.20", 5555, 5)
+
+  val service = system.actorOf(Props(classOf[RiemannMetricsService], settings, "sandbox"))
+  service ! Init
+
+  service ! StringMetric("blue", "test", "color", None, Map())
+  service ! BooleanMetric(true, "test", "boolean", None, Map())
+  service ! DecimalMetric(BigDecimal("22.53"), "test", "decimal", None, Map())
+  (1 to 2).foreach(i => service ! LongMetric(i, "test", "test1", None, Map()))
+
+  val g = Future(Thread.sleep(1500L))
+  Await.ready(g, Duration(3L, TimeUnit.SECONDS))
+
+  system.shutdown()
+}
